0128c9e34f7f383b7db6cd5b7bcf32a35e7e3406
Ayoub AKANOUN <a.akanoun@edu.umi.ac.ma>
2023-01-17 20:24:49 +0100

    First commit

---
+++ musee_backend/src/main/java/com/musee_backend/MuseeBackendApplication.java
@@ -0,0 +1,13 @@
+package com.musee_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MuseeBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MuseeBackendApplication.class, args);
+    }
+
+}
